94331b5ec8b401f034123fd860164dee97162738
ihramap <ihrama13@gmail.com>
2025-02-01 13:24:54 +0700

    Initial commit

--- src/pages/DemoProduct.js
+++ src/pages/DemoProduct.js
@@ -8,7 +8,7 @@ import Notiflix from 'notiflix';
 
 const DemoProduct = (props) => {
 
-    useDocTitle('MLD | Molad e Konsult - Demo our products')
+    useDocTitle('Consultant Statistics')
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
